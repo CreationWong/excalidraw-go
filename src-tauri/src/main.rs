9e0a9976eeedba3e7a9eaa19a03f90acaf5653d8
Creation·Wong <chuangyuwang@creationwong.onmicrosoft.com>
2025-08-17 17:06:28 +0800

    优化 部分代码

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// easy - run
+// fn main() {
+//     excalidraw_go_lib::run()
+// }
+
+use tauri::WindowEvent;
+
 fn main() {
-    excalidraw_go_lib::run()
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running Tauri application");
 }
